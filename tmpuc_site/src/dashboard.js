c6ba0e937d5b0c750b4c4532194e9b6824c9f61f
Elsie-EA-DGO <emeeade-graft-owoo@st.ug.edu.gh>
2024-07-13 12:53:43 +0000

    Page that sign in button of login page leads to

---
+++ tmpuc_site/src/dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Dashboard(){
+  return(
+    <div style={{backgroundColor: '#00005f'}}>
+      <h1 style={{color: 'white'}}>DASHBORD</h1>
+      <h2 style={{color: 'white'}}>Hi User!</h2>
+
+    </div>
+  );
+}
+
+export default Dashboard;
