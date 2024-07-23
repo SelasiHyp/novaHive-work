70197fc28c00bc5fabfb544a669db67de841e917
Elsie-EA-DGO <emeeade-graft-owoo@st.ug.edu.gh>
2024-07-23 05:54:11 +0000

    First commit of Application Form Page

--- tmpuc_site/src/App.js
+++ tmpuc_site/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './LoginPage';
-import Dashboard from './dashboard';
+// import Dashboard from './dashboard';
+import ApplicationForm from './ApplicationFormPage';
 
 function App() {
   return (
@@ -14,7 +15,9 @@ function App() {
         </nav> */}
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          <Route path="/ApplicationFormPage" element={<ApplicationForm />} />
+
         </Routes>
     </Router>
   </>
