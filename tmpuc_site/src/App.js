d94337c6a1281b0f0157c80222c0d72b85190c80
Elsie-EA-DGO <emeeade-graft-owoo@st.ug.edu.gh>
2024-08-16 19:58:31 +0000

    updated app.js file and styles

--- tmpuc_site/src/App.js
+++ tmpuc_site/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './LoginPage';
-// import Dashboard from './dashboard';
-import ApplicationForm from './ApplicationFormPage';
+import LoginPage from './Pages/LoginPage';
+import StudentDashboard from './Pages/Student/StudentDashboard';
+// import StudentDashboard from './Pages/StudentDashboard';
+import ApplicationForm from './Pages/ApplicationFormPage';
+import ResourcesPage from './Pages/Student/ResourcesPage';
+import AssignmentPage from './Pages/Student/AssignmentPage';
+import NotificationPage from './Pages/Student/NotificationPage';
+import CoursePage from './Pages/Student/CoursePage';
+import Overview from './Pages/Student/OverviewPage';
+
 
 function App() {
   return (
@@ -15,8 +22,18 @@ function App() {
         </nav> */}
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
           <Route path="/ApplicationFormPage" element={<ApplicationForm />} />
+          <Route path="/Student/Dashboard" element={<StudentDashboard />} />
+          <Route path="/Student/Resources" element={<ResourcesPage />} />
+          <Route path="/Student/Assignments" element={<AssignmentPage />} />
+          <Route path="/Student/Notifications" element={<NotificationPage />} />
+          <Route path="/:courseId/:pageType" element={<CoursePage />} />
+          <Route path="/Student/:courseId/Overview" element={<Overview/>} />
+
+
+
+          
+
 
         </Routes>
     </Router>
